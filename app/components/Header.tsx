4beed1780038c2502f65bd185c2da4d394bf4aa7
vandilsonbrito <vandilson25@outlook.com>
2024-09-13 21:59:00 -0300

    Feat: create Header component

---
+++ app/components/Header.tsx
@@ -0,0 +1,21 @@
+'use client'
+import Image from 'next/image';
+import Logo from '../../public/logo.png';
+import UserNav from './UserNav';
+
+export default function Header() {
+  return (
+    <header className="w-full h-[6rem] bg-black px-5 md:px-14 flex items-center justify-between" id='/top'>
+        <Image 
+            src={Logo} 
+            alt="Logo"
+            className='w-20'
+        />
+        <h1 className='text-lg font-medium text-white'>The best recommended books for you</h1>
+        <nav className='text-white'>
+            <UserNav/>
+        </nav>
+
+    </header>
+  )
+}

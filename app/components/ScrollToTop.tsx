466bdbd1e9d99699e46d42534ab7c8b66bb73032
vandilsonbrito <vandilson25@outlook.com>
2024-09-13 21:56:57 -0300

    Chore: add logo img

---
+++ app/components/ScrollToTop.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { Link } from "react-scroll";
+import { TbSquareRoundedArrowUp } from "react-icons/tb";
+
+const ScrollToTop = () => {
+  const [scrollY, setScrollY] = useState(0);
+
+  const handleScroll = () => {
+    setScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <>
+      <Link to="/top" href="" smooth={true} duration={500} className={`${scrollY > 570 ? 'visible' : 'hidden'}`}>
+        <TbSquareRoundedArrowUp className="hidden xl:block fixed bottom-10 right-6 text-4xl text-[#000000e5] dark:text-white cursor-pointer"/>
+      </Link>
+    </>
+  );
+};
+
+export default ScrollToTop;

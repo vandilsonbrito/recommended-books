fd1cf013d0ac1da26a3ea69d533f2ce69cdf552c
vandilsonbrito <vandilson25@outlook.com>
2024-09-15 00:16:56 -0300

    Chore: create interfaces file and add BooksDataType interface

---
+++ utils/interfaces.ts
@@ -0,0 +1,9 @@
+export interface BooksDataType {
+    id: number,
+    title: string,
+    genre: string[],
+    author: string,
+    imageUrl: string,
+    linkToBuy: string,
+    rating: string
+};

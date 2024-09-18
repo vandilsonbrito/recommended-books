5ed5b5b994176e234dfa084e1f040ef00f8eaa3c
vandilsonbrito <vandilson25@outlook.com>
2024-09-18 15:32:14 -0300

    Chore: add userDB to AuthContextType

--- context/AuthContext.ts
+++ context/AuthContext.ts
@@ -1,9 +1,12 @@
 'use client';
 import { createContext, useContext } from 'react';
 import { User } from 'firebase/auth';
+import { Database } from '@/utils/interfaces';
+
 
 interface AuthContextType {
     userAuth: User | null;
+    userDB: Database | null;
     logout: () => void;
 }
 

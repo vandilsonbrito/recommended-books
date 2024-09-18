3b04518c64be00c96bacfcb95639a522b67b2a2d
vandilsonbrito <vandilson25@outlook.com>
2024-09-18 15:31:20 -0300

    Chore: sync user's DB and add to provider

--- context/AuthProvider.tsx
+++ context/AuthProvider.tsx
@@ -3,6 +3,9 @@ import { useEffect, useState, ReactNode } from 'react';
 import { onAuthStateChanged, User, signOut } from 'firebase/auth';
 import { AuthContext } from './AuthContext';
 import { auth } from '@/firebase/firebaseAuthConfig';
+import { database } from '@/firebase/firebaseDBConfig';
+import { onValue, ref } from 'firebase/database';
+import { Database } from '@/utils/interfaces';
 
 interface AuthContextProviderProps {
     children: ReactNode;
@@ -14,6 +17,7 @@ export const AuthContextProvider: React.FC<AuthContextProviderProps> = ({
 }) => {
     const [userAuth, setUserAuth] = useState<User | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
+    const [userDB, setUserDB] = useState<Database | null>(null);
      
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (authUserCredentials: User | null) => {
@@ -24,6 +28,14 @@ export const AuthContextProvider: React.FC<AuthContextProviderProps> = ({
         return () => unsubscribe();
     }, []);
 
+    useEffect(() => {
+        const dbRef = ref(database, 'users/' + userAuth?.uid);
+        onValue(dbRef, (snapshot) => {
+            const data = snapshot.val();
+            setUserDB(data);
+        });
+    }, [userAuth])
+
     async function logout() {
         let result = null,
         error = null;
@@ -33,12 +45,11 @@ export const AuthContextProvider: React.FC<AuthContextProviderProps> = ({
         catch(e) {
             error = e;
         }
-
         return { result, error };
     }
 
     return (
-        <AuthContext.Provider value={{ userAuth, logout }}>
+        <AuthContext.Provider value={{ userAuth, logout, userDB }}>
             {  loading 
                 ? 
                         <div className="w-full h-full min-h-screen flex flex-col justify-center items-center">

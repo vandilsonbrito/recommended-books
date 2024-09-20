fdb9ae836368e8a459960ea9c7939ba4eed685b2
vandilsonbrito <vandilson25@outlook.com>
2024-09-19 23:22:07 -0300

    Feat: puts logo inside the link tag and with navigation to the books page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,21 +2,23 @@
 import Image from 'next/image';
 import Logo from '../../public/logo.png';
 import UserNav from './UserNav';
+import Link from 'next/link';
 
 export default function Header() {
     
     return (
       <header className="w-full h-[6rem] bg-black px-5 md:px-14 flex items-center justify-between" id='/top'>
-          <Image 
-              src={Logo} 
-              alt="Logo"
-              className='w-16 md:w-20'
-          />
-          <h1 className='hidden md:block text-lg font-medium text-white'>The best recommended books for you</h1>
-          <nav className='text-white'>
-              <UserNav/>
-          </nav>
-
+            <Link href="/books">
+                <Image 
+                    src={Logo} 
+                    alt="Logo"
+                    className='w-16 md:w-20'
+                />
+            </Link>
+            <h1 className='hidden md:block text-lg font-medium text-white'>The best recommended books for you</h1>
+            <nav className='text-white'>
+                <UserNav/>
+            </nav>
       </header>
     )
 }

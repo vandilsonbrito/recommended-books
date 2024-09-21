2cf87183f1ef4a08ef3f6d78fc962fd2ea8a9b4a
vandilsonbrito <vandilson25@outlook.com>
2024-09-20 23:25:19 -0300

    Chore: update card width

--- app/components/CardSkeleton.tsx
+++ app/components/CardSkeleton.tsx
@@ -2,12 +2,12 @@ import { IoIosStar } from "react-icons/io";
 
 function CardSkeleton() {
     return (
-      <div className="w-[249] h-[400px] border border-gray-300 rounded-lg overflow-hidden">
+      <div className="w-[209] h-[400px] border border-gray-300 rounded-lg overflow-hidden">
         <div className="w-full h-[250px] rounded-sm bg-gray-200 opacity-50 animate-pulse flex flex-col justify-center items-center">
             <div className="w-[170px] h-[220px] animate-pulse bg-gray-400 "/>
         </div>
 
-        <div className="w-[249px] h-[150px] bg-white opacity-50 rounded-sm animate-pulse px-4 py-3">
+        <div className="w-[240px] h-[150px] bg-white opacity-50 rounded-sm animate-pulse px-4 py-3">
             <div className="w-[220px] h-[8px] bg-gray-400 opacity-50 mb-2 rounded-sm animate-pulse" />
             <div className="w-[200px] h-[8px] bg-gray-400 opacity-50 rounded-sm animate-pulse" />
             <div className="w-[170px] h-[8px] bg-gray-300 opacity-50 mt-3 rounded-sm animate-pulse" />

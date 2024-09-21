a59748aed15fdf5cb08cb8d84e0717beca8550ba
vandilsonbrito <vandilson25@outlook.com>
2024-09-21 20:02:56 -0300

    Chore: add font-size for star icons

--- app/components/CardSkeleton.tsx
+++ app/components/CardSkeleton.tsx
@@ -4,7 +4,7 @@ function CardSkeleton() {
     return (
       <div className="w-[209] h-[400px] border border-gray-300 rounded-lg overflow-hidden">
         <div className="w-full h-[250px] rounded-sm bg-gray-200 opacity-50 animate-pulse flex flex-col justify-center items-center">
-            <div className="w-[170px] h-[220px] animate-pulse bg-gray-400 "/>
+            <div className="w-[160px] h-[220px] animate-pulse bg-gray-400 "/>
         </div>
 
         <div className="w-[240px] h-[150px] bg-white opacity-50 rounded-sm animate-pulse px-4 py-3">
@@ -12,7 +12,7 @@ function CardSkeleton() {
             <div className="w-[200px] h-[8px] bg-gray-400 opacity-50 rounded-sm animate-pulse" />
             <div className="w-[170px] h-[8px] bg-gray-300 opacity-50 mt-3 rounded-sm animate-pulse" />
 
-            <div className="w-full flex mt-4 gap-1 animate-pulse">
+            <div className="w-full flex mt-4 gap-1 animate-pulse text-base">
                 <IoIosStar/>
                 <IoIosStar/>
                 <IoIosStar/>

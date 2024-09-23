ea01703279e2ebc2a107da39e79b9a850233393d
vandilsonbrito <vandilson25@outlook.com>
2024-09-23 12:48:10 -0300

    Chore: change footer styles adding fixed height

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className='pt-3 pb-2 border-t '>
+    <footer className='border-t w-full h-12 flex flex-col justify-center items-center text-base'>
         <p className="text-sm md:text-base text-center">Developed by <a target="_blank" className="underline" href="https://vandilson-portfolio.vercel.app/">Vandilson Brito</a></p>
     </footer>
   )

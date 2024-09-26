95b8548b2bafe74bd0b02dd71853bd0c5aead235
vandilsonbrito <vandilson25@outlook.com>
2024-09-26 19:33:27 -0300

    Feat: add useTranslations hook and appropriate language variables

--- app/components/FilterComponent.tsx
+++ app/components/FilterComponent.tsx
@@ -11,13 +11,16 @@ import {
 } from "@/components/ui/dropdown-menu";
 import useGlobalStore from "@/utils/store";
 import { useAuthContext } from "@/context/AuthContext";
+import { useTranslations } from 'next-intl';
 
 
 export default function FilterComponent() {
 
     const { userDB } = useAuthContext();
     const { userSelectedGenres, addUserSelectedGenres, removeUserSelectedGenres } = useGlobalStore();
-
+    const t = useTranslations('BooksPage');
+    const tGenres = useTranslations('FavoriteGenresPage');
+    const [isBasedOnPreferences, setIsBasedOnPreferences] = React.useState(false);
 
     React.useEffect(() => {
         if(userDB?.preferences && userSelectedGenres.length === 0){
@@ -40,67 +43,67 @@ export default function FilterComponent() {
             { isBasedOnPreferences && <p className="w-full text-base sm:text-base text-left">{t("noFilterMessage")}</p> }
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
-                    <Button variant="outline">Filter</Button>
+                    <Button variant="outline">{t("filter")}</Button>
                 </DropdownMenuTrigger>
 
                 <DropdownMenuContent className="w-56">
-                    <DropdownMenuLabel>Genres</DropdownMenuLabel>
+                    <DropdownMenuLabel>{t("genres")}</DropdownMenuLabel>
                     <DropdownMenuSeparator />
 
                     <DropdownMenuCheckboxItem
                         checked={Boolean(userSelectedGenres.some((genre) => genre.includes('self-development')))}
                         onCheckedChange={(e) => (e ? addUserSelectedGenres(['self-development']) : removeUserSelectedGenres(['self-development']) )}
-                    > Self-development</DropdownMenuCheckboxItem>
+                    >{tGenres("genres.genre1")}</DropdownMenuCheckboxItem>
 
                     <DropdownMenuCheckboxItem
                         checked={Boolean(userSelectedGenres.some((genre) => genre.includes('mindset')))}
                         onCheckedChange={(e) => (e ? addUserSelectedGenres(['mindset']) : removeUserSelectedGenres(['mindset']) )}
-                    >Mindset</DropdownMenuCheckboxItem>
+                    >{tGenres("genres.genre2")}</DropdownMenuCheckboxItem>
 
                     <DropdownMenuCheckboxItem
                         checked={Boolean(userSelectedGenres.some((genre) => genre.includes('scientific')))}
                         onCheckedChange={(e) => (e ? addUserSelectedGenres(['scientific']) : removeUserSelectedGenres(['scientific']) )}
-                    >Scientific</DropdownMenuCheckboxItem>
+                    >{tGenres("genres.genre3")}</DropdownMenuCheckboxItem>
                     
                     <DropdownMenuCheckboxItem
                         checked={Boolean(userSelectedGenres.some((genre) => genre.includes('biography')))}
                         onCheckedChange={(e) => (e ? addUserSelectedGenres(['biography']) : removeUserSelectedGenres(['biography']) )}
-                    >Biography</DropdownMenuCheckboxItem>
+                    >{tGenres("genres.genre4")}</DropdownMenuCheckboxItem>
                     
                     <DropdownMenuCheckboxItem
                         checked={Boolean(userSelectedGenres.some((genre) => genre.includes('health')))}
                         onCheckedChange={(e) => (e ? addUserSelectedGenres(['health']) : removeUserSelectedGenres(['health']) )}
-                    >Health</DropdownMenuCheckboxItem>
+                    >{tGenres("genres.genre5")}</DropdownMenuCheckboxItem>
 
                     <DropdownMenuCheckboxItem
                         checked={Boolean(userSelectedGenres.some((genre) => genre.includes('success')))}
                         onCheckedChange={(e) => (e ? addUserSelectedGenres(['success']) : removeUserSelectedGenres(['success']) )}
-                    >Success</DropdownMenuCheckboxItem>
+                    >{tGenres("genres.genre6")}</DropdownMenuCheckboxItem>
 
                     <DropdownMenuCheckboxItem
                         checked={Boolean(userSelectedGenres.some((genre) => genre.includes('languages')))}
                         onCheckedChange={(e) => (e ? addUserSelectedGenres(['languages']) : removeUserSelectedGenres(['languages']) )}
-                    >Languages</DropdownMenuCheckboxItem>
+                    >{tGenres("genres.genre7")}</DropdownMenuCheckboxItem>
 
                     <DropdownMenuCheckboxItem
                         checked={Boolean(userSelectedGenres.some((genre) => genre.includes('finance')))}
                         onCheckedChange={(e) => (e ? addUserSelectedGenres(['finance']) : removeUserSelectedGenres(['finance']) )}
-                    >Finance</DropdownMenuCheckboxItem>
+                    >{tGenres("genres.genre8")}</DropdownMenuCheckboxItem>
 
                     <DropdownMenuCheckboxItem
                         checked={Boolean(userSelectedGenres.some((genre) => genre.includes('software-development')))}
                         onCheckedChange={(e) => (e ? addUserSelectedGenres(['software-development']) : removeUserSelectedGenres(['software-development']) )}
-                    >Software-development</DropdownMenuCheckboxItem>
+                    >{tGenres("genres.genre9")}</DropdownMenuCheckboxItem>
 
                     <DropdownMenuCheckboxItem
                         checked={Boolean(userSelectedGenres.some((genre) => genre.includes('marketing')))}
                         onCheckedChange={(e) => (e ? addUserSelectedGenres(['marketing']) : removeUserSelectedGenres(['marketing']) )}
-                    >Marketing</DropdownMenuCheckboxItem>
+                    >{tGenres("genres.genre10")}</DropdownMenuCheckboxItem>
 
                     <DropdownMenuCheckboxItem
                         checked={Boolean(userSelectedGenres.some((genre) => genre.includes('communication')))}
                         onCheckedChange={(e) => (e ? addUserSelectedGenres(['communication']) : removeUserSelectedGenres(['communication']) )}
-                    >Communication</DropdownMenuCheckboxItem>
+                    >{tGenres("genres.genre11")}</DropdownMenuCheckboxItem>
     
                 </DropdownMenuContent>
             </DropdownMenu>

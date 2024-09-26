cdb5a6c2b03034a25d85ac41f655c7da45af02a5
vandilsonbrito <vandilson25@outlook.com>
2024-09-26 19:25:36 -0300

    Build: add next-intl config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,19 @@
 /** @type {import('next').NextConfig} */
+import createNextIntlPlugin from 'next-intl/plugin';
+ 
+const withNextIntl = createNextIntlPlugin();
+ 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'm.media-amazon.com',
-            port: '',
-          }
-        ],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'm.media-amazon.com',
+          port: '',
+        }
+      ],
     },
 };
-
-export default nextConfig;
+ 
+export default withNextIntl(nextConfig);
